17fc1e2a9d2a2e0e9a072f2a7017102c324e2521
its-asimiqbal <its.asim.iqball@gmail.com>
2023-12-06 15:21:36 +0500

    files updated

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -12,6 +12,7 @@ class _MyHomeScreen1State extends State<MyHomeScreen1> {
   @override
   Widget build(BuildContext context) {
     TextTheme _textTheme = Theme.of(context).textTheme;
+    bool isDark = Theme.of(context).brightness == Brightness.dark;
 
     return Scaffold(
       appBar: AppBar(
@@ -30,54 +31,58 @@ class _MyHomeScreen1State extends State<MyHomeScreen1> {
         child: const Icon(Icons.add),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/profile1.jpeg',
-              height: 200,
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Asim Iqbal',
-              style: _textTheme.headlineMedium?.copyWith(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+             const Icon(
+                Icons.login,
+                size: 120,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              '@yourusername',
-              style: _textTheme.bodyMedium,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text('This is a simple status'),
-            const SizedBox(
-              height: 10,
-            ),
-            const TextField(),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Click Here'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Current theme: ${Theme.of(context).brightness == Brightness.dark ? 'Dark' : 'Light'}',
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Asim Iqbal',
+                style: _textTheme.headlineMedium?.copyWith(
+                  color: isDark ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '@yourusername',
+                style: _textTheme.bodyMedium,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('This is a simple status'),
+              const SizedBox(
+                height: 20,
+              ),
+              const TextField(),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Click Here'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Current theme: ${Theme.of(context).brightness == Brightness.dark ? 'Dark' : 'Light'}',
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -27,8 +27,59 @@ ThemeData lightTheme = ThemeData(
       ),
     ),
   ),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: BorderSide.none,
+    ),
+    fillColor: Colors.grey.withOpacity(0.3),
+    filled: true,
+  ),
 );
 
 ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Colors.white,
+  ),
+  switchTheme: SwitchThemeData(
+    trackColor: MaterialStateProperty.all<Color?>(
+      Colors.grey,
+    ),
+    thumbColor: MaterialStateProperty.all<Color?>(
+      Colors.white,
+    ),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all(
+        const EdgeInsetsDirectional.symmetric(
+          horizontal: 40,
+          vertical: 15,
+        ),
+      ),
+      backgroundColor: MaterialStateProperty.all<Color>(
+        Colors.white,
+      ),
+      foregroundColor: MaterialStateProperty.all<Color>(
+        Colors.black,
+      ),
+      overlayColor: MaterialStateProperty.all<Color>(
+        Colors.black26,
+      ),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(20),
+      borderSide: BorderSide.none,
+    ),
+    fillColor: Colors.white.withOpacity(0.3),
+    filled: true,
+  ),
 );
